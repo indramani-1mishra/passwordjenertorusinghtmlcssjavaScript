6826d3d746af7e15d8e473f64af6dacf23b08c92
RAJ <rajmishra.ydd@gmail.com>
2024-12-19 14:33:15 +0530

    ist version of my code

--- passwordjenter.js
+++ passwordjenter.js
@@ -1,23 +1,99 @@
-let range= document.getElementById("range");
+let lowerCase = 'abcdefghijklmnopqrstuvwxyz';
+
+let upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+let numbers = '0123456789';
+
+let specialCharacters = '!@#$%^&*()_+=-{}[]|:;<>,.?/~`';
+let allCharacters ='';
 let rengeOfPassword;
 
-function generatePassword(rangeofpassword) {
-    let password = '';
-     let characters='abcdefghijklmnopqrstuvwxyzABCDEFGH1234567890#$/?<>,!@';
-     let length = rangeofpassword;
-     for(let i=0; i<length; i++) {
-        password += characters.charAt(Math.floor(Math.random() * characters.length));
-     }
-    return password;
-}
-//display the jenerted password into the input box
-  let generatebutton = document.getElementById('btng');
-  generatebutton.addEventListener('click', () => {
-    let password = generatePassword(rengeOfPassword);
-    document.getElementById('display').value = password;
-  });
 
-    
+let uppercasevalue= document.getElementById('upper');
+let yesNoHandler;
+
+
+//let text;
+   uppercasevalue.addEventListener('click',(event)=>
+    {  yesNoHandler=event.target.checked;
+      
+       if(yesNoHandler)
+        {
+          allCharacters+= upperCase;  
+          console.log("upparcase includeed");
+        }
+        else
+        {
+            allCharacters=allCharacters.replace(upperCase,'');  
+          console.log("upparcase removed");
+        }
+    });
+    let lowercasevalue= document.getElementById('lower');
+    lowercasevalue.addEventListener('click',(event)=>
+    { yesNoHandler=event.target.checked;
+        if(yesNoHandler)
+        {
+          allCharacters+= lowerCase;  
+          console.log("lowercase includeed");
+        }
+        else
+        {
+            allCharacters=allCharacters.replace(lowerCase,'');  
+          console.log("lowercase removed");
+        }
+    });
+    let numbersvalue= document.getElementById('number');
+    numbersvalue.addEventListener('click',(event)=>
+    { yesNoHandler=event.target.checked;
+        if(yesNoHandler)
+        {
+          allCharacters+= numbers;  
+          console.log("numbers included");
+        }
+        else
+        {
+            allCharacters=allCharacters.replace(numbers,'');  
+          console.log("numbers removed");
+        }
+    });
+    let specialvalue= document.getElementById('special');
+    specialvalue.addEventListener('click',(event)=>
+    { yesNoHandler=event.target.checked;
+        if(yesNoHandler)
+        {
+          allCharacters+= specialCharacters;  
+          console.log("special characters included");
+        }
+        else
+        {
+            allCharacters=allCharacters.replace(specialCharacters,'');  
+          console.log("special characters removed");
+        }
+    });
+   
+    function  generatePassword(rangeofpassword) {
+        let password = '';
+
+        for (let i = 0; i < rangeofpassword; i++) {
+            password += allCharacters.charAt(Math.floor(Math.random() * allCharacters.length));
+        }
+        return password;
+    }
+     
+    let generateButton = document.getElementById('btng');
+    generateButton.addEventListener('click', () => {
+        
+        if (allCharacters === '') {
+            alert('First select the options you want to include in the password.');
+        } else {
+            // Generate the password only if characters are selected
+           // let passwordLength=9;
+           // let password = generatePassword(passwordLength);
+            let password = generatePassword(rengeOfPassword);
+            document.getElementById('display').value = password;
+            console.log(password);
+        }
+    });    
 
 range.addEventListener('input', (event) => {
     rengeOfPassword = event.target.value;
@@ -25,7 +101,6 @@ range.addEventListener('input', (event) => {
     let generatePassword1= generatePassword(rengeOfPassword);
 console.log("generate password: " + generatePassword1);
        
-
     }) 
 
 // function for copy the password to the clipboard
@@ -39,3 +114,5 @@ copybtn.addEventListener("click", ()=> {
   document.execCommand("copy");
   alert("Password copied to clipboard!"+copyText.value);
 });
+
+// try to fecth the value of checkbox
