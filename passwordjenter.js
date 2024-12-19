895ae94d2e9d03809ce53a7a6a689ae04b671bf7
RAJ <rajmishra.ydd@gmail.com>
2024-12-19 07:34:41 +0530

    first version of my code

---
+++ passwordjenter.js
@@ -0,0 +1,30 @@
+let range= document.getElementById("range");
+let rengeOfPassword;
+
+function generatePassword(rangeofpassword) {
+    let password = '';
+     let characters='abcdefghijklmnopqrstuvwxyzABCDEFGH1234567890#$/?<>,!@';
+     let length = rangeofpassword;
+     for(let i=0; i<length; i++) {
+        password += characters.charAt(Math.floor(Math.random() * characters.length));
+     }
+    return password;
+}
+//display the jenerted password into the input box
+  let generatebutton = document.getElementById('btng');
+  generatebutton.addEventListener('click', () => {
+    let password = generatePassword(rengeOfPassword);
+    document.getElementById('display').value = password;
+  });
+
+
+range.addEventListener('input', (event) => {
+    rengeOfPassword = event.target.value;
+    console.log("password length: " + rengeOfPassword);
+    let generatePassword1= generatePassword(rengeOfPassword);
+console.log("generate password: " + generatePassword1);
+       
+
+    }) 
+
+
